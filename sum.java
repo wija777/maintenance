9ee48158260f90f2efaeb55a7e8d817dcf621ea8
Wijdan Isam <Wijdan Isam>
2018-02-06 11:14:20 +0200

    jhvjisk

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+    public static void main(String[]a){
+    
+        System.out.println(sum(1,1));
+    }
+    public static int sum(int x, int y){
+        return  x+y;
+    }
+}
